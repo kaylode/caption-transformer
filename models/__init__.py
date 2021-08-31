a2a0b8fb142f643472071bca878f94c134a9cd39
kaylode <kayp.kieran@gmail.com>
2021-08-31 13:01:45 +0700

    add get transformer bottom up

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .transformer import Transformer
+from .transformer import Transformer, TransformerBottomUp
 from .captioning import Captioning
 
 def get_transformer_model(patch_size, trg_vocab):
@@ -17,3 +17,18 @@ def get_transformer_model(patch_size, trg_vocab):
     }
 
     return Transformer(**transformer_config)
+
+def get_transformer_model(bottom_up_dim, trg_vocab):
+
+    transformer_config = {
+        'feat_dime':       bottom_up_dim,
+        'trg_vocab':        trg_vocab, 
+        "d_model":          768, 
+        "d_ff":             3072,
+        "N_enc":            6,
+        "N_dec":            6,
+        "heads":            8,
+        "dropout":          0.3,
+    }
+
+    return TransformerBottomUp(**transformer_config)
